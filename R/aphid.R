40eec247ab966568ddbac17936006de041f7fae6
shaunpwilkinson <shaunpwilkinson@gmail.com>
2017-02-14 16:47:41 +0900

    Updated NAMESPACE to import ape functions

--- R/aphid.R
+++ R/aphid.R
@@ -1,11 +1,11 @@
 #' aphid: an R package for analysis with profile hidden Markov models.
 #'
-#'  Package 'aphid' contains functions for building profile hidden Markov models
-#'  for biological sequence analysis
+#' Package 'aphid' contains functions for building profile hidden Markov models
+#' for biological sequence analysis.
 #'
 #' @section aphid functions:
 #'
 #' @docType package
 #' @name aphid
-
+NULL
 
---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' Substutution matrices.
+#'
+#' A dataset containing several of the more popular substitution scoring matrices
+#' for DNA and amino acids.
+#'
+#' @format A list of...:
+#' \describe{
+#'   \item{PAM}{the PAM martices from... }
+#'   \item{BLOSUM}{the BLOSUM matrices from...}
+#' }
+#' @source \url{ftp://ftp.ncbi.nih.gov/blast/matrices/}
+"substitution_matrices"
